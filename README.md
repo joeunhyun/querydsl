44067c5455cc5a865039cbbd83cc4f959ca3aa1e
EunHyun <dmsgusdl31@gmail.com>
2022-02-21 21:30:25 +0900

    QueryDsl 사용, 검색조건, 결과조회, 정렬

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# querydsl
+# querydsl study
+
+## 환경
+- spring Boot 2.6.0
+- gradle 7.4
+- queryDsl 5.0.0
+
+## 참고
+- Unable to load class 'com.mysema.codegen.model.Type'. gradle 오류 발생   
+ -> gradle 5.0 이상부터는 옵션을 추가해야함 (cc. https://www.inflearn.com/questions/355723)
+- Q 파일 업로드 경로 확인
+  - build.gradle 확인
+  - def querydslDir = "$buildDir/generated/querydsl"
+
+## JPQL 장점
+1. 컴파일 시 오류가 발생한다.
+2. 자동으로 파라미터 바인딩 처리를 해준다.
+
+- Query DSL 사용 시 JPQL 쿼리문 보고 싶을 때
+  jpa:properties:hibernate:use_sql_comments : true 
+
+## JPQL이 제공하는 모든 검색 조건 제공
+`member.username.eq("member1") // username = 'member1'`
+`member.username.ne("member1") //username != 'member1'`
+`member.username.eq("member1").not() // username != 'member1'`
+`member.username.isNotNull() //이름이 is not null`
+`member.age.in(10, 20) // age in (10,20)`
+`member.age.notIn(10, 20) // age not in (10, 20)`
+`member.age.between(10,30) //between 10, 30`
+`member.age.goe(30) // age >= 30`
+`member.age.gt(30) // age > 30`
+`member.age.loe(30) // age <= 30`
+`member.age.lt(30) // age < 30`
+`member.username.like("member%") //like 검색`
+`member.username.contains("member") // like ‘%member%’ 검색`
+`member.username.startsWith("member") //like ‘member%’ 검색`
+
+## 결과 조회
+- fetch() : 리스트 조회, 데이터 없으면 빈 리스트 반환
+- fetchOne() : 단 건 조회
+  - 결과가 없으면 : null
+  - 결과가 둘 이상이면 : com.querydsl.core.NonUniqueResultException
+- fetchFirst() : limit(1).fetchOne()
+- fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행
+- fetchCount() : count 쿼리로 변경해서 count 수 조회
---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,159 @@
+package study.querydsl;
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+ class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+     void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+     void startJPQL(){
+        //member1을 찾아라
+        String qlString = "select m from Member m " +
+                "where m.username = :username";
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+     void startQuerydsl(){
+//      멀티쓰레드 환경에서 동시성 문제 없이 사용 가능, 필드로 빼길 권장 (소스 줄어듬)
+//      JPAQueryFactory queryFactory = new JPAQueryFactory(em); //위로 빼도 가능
+
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) //파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+     void startOtherWayQuerydsl(){
+        //import static study.querydsl.entity.QMember.member
+        //jpql builder 역할이라고 생각하면 됨
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) //파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+     void search(){
+        Member findMember = queryFactory.selectFrom(QMember.member)
+//                .where(QMember.member.username.eq(("member1")).and(QMember.member.age.eq(10)))
+                //위에 케이스와 동일
+                //아래는 and 만 있을 때 사용하길 권장
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+     void resultFetch(){
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(QMember.member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        results.getTotal();
+
+        List<Member> content = results.getResults();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막 출력(nulls last)
+     */
+    @Test
+     void sort(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+}
